33367c3a5b25b058029a069f35328832fdc41680
DangCoop <105824586+DangCoop@users.noreply.github.com>
2024-07-10 00:21:31 -0400

    Add working file

---
+++ sandbox.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+# Open the URL
+driver.get('https://www.target.com/login?client_id=ecom-web-1.0.0&ui_namespace=ui-default&back_button_action=browser&keep_me_signed_in=true&kmsi_default=false&actions=create_session_signin')
+
+sing_in_text = driver.find_element(By.XPATH, "//h1//span[text()='Sign into your Target account']")
+
+sleep(2)
